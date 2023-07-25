f16f7ae994ccd5405e0c2dfded67e78f281a19e5
Jeferson Ferreira <jefersonpks@gmail.com>
2023-07-24 23:45:34 -0300

    Implementando o template da página de detalhes da receita

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from recipes.views import *
 
+from . import views
 
 urlpatterns = [
-    path('', home)
+    path('', views.home),
+    path('recipe/<int:id>/', views.recipe)
 ]
