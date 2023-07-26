846ad0517e04f6cd0f4469eba1dbfc23d04b3234
Jeferson Ferreira <jefersonpks@gmail.com>
2023-07-26 09:16:20 -0300

    Ajustes nas views e nos templates. Inclusão da lib Faker para ajudar nos testes.

--- core/settings.py
+++ core/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
