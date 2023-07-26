92a1e0863ee12c4d10dfe4a506cbd2b91269d1ad
Jeferson Ferreira <jefersonpks@gmail.com>
2023-07-26 14:36:35 -0300

    Adição da pasta media e registro dos models a área administrativa do Django

--- core/settings.py
+++ core/settings.py
@@ -124,6 +124,10 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = 'static_files/'
 
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
