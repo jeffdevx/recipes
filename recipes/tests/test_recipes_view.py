88a25c6f555b13d170c51a3e1057da7c45f5466e
Jeferson Ferreira <jefersonpks@gmail.com>
2023-08-24 22:45:58 -0300

    Adicionando mais testes: criando fixtures para um teste único usando (Recipe, Category e Author/User)

--- recipes/tests/test_recipes_view.py
+++ recipes/tests/test_recipes_view.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse, resolve
+from recipes import models
 from recipes import views
+from recipes.models import Category, Recipe
 
 
 class RecipeViewTest(TestCase):
@@ -11,6 +14,37 @@ class RecipeViewTest(TestCase):
     def test_recipes_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
         self.assertIs(view.func, views.category)
+
+    def test_recipes_home_template_loads_recipes(self):
+        category = Category.objects.create(name='Category')
+        author = User.objects.create(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )        
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation Steps',
+            preparation_steps_is_html=False,
+            is_published=True,
+        )        
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+        self.assertIn('Recipe Title', content)
+        self.assertIn('10 Minutos', content)
+        self.assertIn('5 Porções', content)
+        self.assertEqual(len(response_context_recipes), 1)
 
     def test_recipes_category_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(
